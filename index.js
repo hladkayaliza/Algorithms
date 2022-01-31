382ab1e93e1a0b4df4eba3b7f552bddc5249a410
Liza Hladkaya <liza.hladkaya@gmail.com>
2022-01-31 11:20:52 +0300

    Graph

--- index.js
+++ index.js
@@ -1,101 +1,79 @@
+(() => {
 
-function calculateSum(array) {
-    for(let i = 0; i < array.length; i++) {
-        if (array.length === 1) {
-            return array[i];
-        }
-        let baseCase = array[i];
-        array.shift();
+})();
 
-        return  baseCase + calculateSum(array);
-    }
-}
-
-function calculateItems(array) {
-    for (let i = 0; i < array.length + 1; i++) {
-        if (!array.length) {
-            return 0;
-        }
-        array.shift();
-
-        return 1 + calculateItems(array);
-    }
-}
-
-function finMaxNode(list, max) {
-    let currentNode = list.head;
-    let maxValue = max > list.head.data ? max : list.head.data;
-
-    while (currentNode.next) {
-        if (currentNode.data > maxValue) {
-            maxValue = currentNode.data;
-        }
-        let nextNode = currentNode.next;
-        list.delete(currentNode);
-        currentNode = nextNode;
-        return finMaxNode(list, maxValue);
-    }
-    return maxValue;
-}
-
-
-class Node {
-    constructor(data) {
-        this.next = null;
-        this.data = data;
-    }
-}
-
-class List {
+class Graph {
     constructor() {
-        this.head = null;
-        this.length = 0;
+        this.nodes = new Map();
     }
 
-    add(data) {
-        const node = new Node(data);
-        if (!this.head) {
-            this.head = node;
-            return;
-        }
+    addNode(node) {
+        this.nodes.set(node, []);
+    };
 
-        let currentNode = this.head;
-        while (currentNode.next) {
-            currentNode = currentNode.next;
-        }
-        currentNode.next = node;
+
+    addEdge(source, destination) {
+        this.nodes.get(source).push(destination);
+        this.nodes.get(destination).push(source);
+    };
+
+    getIndexAndRemoveItem(item, list) {
+        const index = list.indexOf(item);
+        list.splice(index, 1);
     }
 
-    delete(node) {
-        if(this.head !== null) {
-            let previousNode = this.head;
-            let currentNode = this.head.next;
-            if (previousNode.data === node.data) {
-                this.head = currentNode;
-                return;
-            }
 
-            while (currentNode.data !== node.data) {
-                if (!currentNode.next) {
-                    previousNode = currentNode;
-                    currentNode = currentNode.next;
+    removeNode(node) {
+        let neighbors = this.nodes.get(node);
+        for(let neighbor of neighbors) {
+            let adjacencyListOfNeighbor = this.nodes.get(neighbor);
+            this.getIndexAndRemoveItem(node, adjacencyListOfNeighbor)
+        }
+        this.nodes.delete(node);
+    };
+    removeEdge(source, destination) {
+        let adjacencyListOfSource = this.nodes.get(source);
+        let adjacencyListOfDestination = this.nodes.get(destination);
+
+        this.getIndexAndRemoveItem(source, adjacencyListOfDestination);
+        this.getIndexAndRemoveItem(destination, adjacencyListOfSource);
+    };
+    depthFirstSearch(startingNode) {
+        let visitedNode = [];
+        this.dfsRecursion(startingNode, visitedNode);
+    };
+
+    dfsRecursion(currentNode, visitedNode) {
+        visitedNode[currentNode] = true;
+        console.log(currentNode);
+        let adjacencyListOfCurrentNode = this.nodes.get(currentNode);
+        for (let node of adjacencyListOfCurrentNode) {
+            if (!visitedNode[node]) this.dfsRecursion(node, visitedNode);
+        }
+    }
+
+    breadthFirstSearch(startingNode) {
+        let visitedNode = [];
+        let queue = [];
+        visitedNode[startingNode] = true;
+        queue.push(startingNode);
+        while (queue.length > 0) {
+            const currentNode = queue.shift();
+
+            console.log(currentNode);
+
+            const adjacencyListOfCurrentNode = this.nodes.get(currentNode);
+
+            for (let node of adjacencyListOfCurrentNode) {
+                if (!visitedNode[node]) {
+                    visitedNode[node] = true;
+                    queue.push(node);
                 }
             }
-            previousNode.next = currentNode.next
         }
-    }
+    };
+    display() {};
 }
 
-(() => {
-    console.log(calculateSum([1,3,5]));
 
-    console.log(calculateItems([1,3,5]));
-
-    let list = new List();
-    list.add(8);
-    list.add(10);
-    list.add(50);
-    list.add(25);
-    list.add(3);
-    console.log(finMaxNode(list, 0));
-})();
+// https://dev.to/codebondco/implementation-of-a-graph-javascript-4jin
